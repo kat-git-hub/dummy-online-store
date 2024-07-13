dfb9eef24d6b7a7f884eb6bc9528f944aac8f419
H P <humanpizzza@gmail.com>
2024-07-13 22:10:26 +0200

    add headless

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -2,6 +2,13 @@ import time
 import pytest
 from selenium.webdriver.common.by import By
 from demoblaze import Demoblaze
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-gpu')
 
 
 @pytest.fixture
--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,6 +1,13 @@
 import pytest
 from demoblaze import Demoblaze
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-gpu')
 
 
 @pytest.fixture
