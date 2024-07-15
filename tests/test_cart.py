7318978514bbbd3500fc501f2d3153f5e42674ff
H P <humanpizzza@gmail.com>
2024-07-15 13:47:08 +0200

    edit tests

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -21,7 +21,7 @@ def test_navigate_to_category(demoblaze):
 def test_select_product(demoblaze):
     demoblaze.navigate_to_category("Laptops")
     demoblaze.select_product("Dell i7 8gb")
-    time.sleep(2)
+    time.sleep(3)
     product_title = demoblaze.driver.find_element(By.XPATH, "//h2[contains(text(), 'Dell i7 8gb')]")
     assert product_title is not None
 
