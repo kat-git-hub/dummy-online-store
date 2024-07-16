4796017f4615473a3f53fa595f14a43e0c0434fb
H P <humanpizzza@gmail.com>
2024-07-16 21:03:39 +0200

    edit  coverage settings

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 class Config:
     BASE_URL = "https://www.demoblaze.com/index.html"
-    #DRIVER_PATH = "drivers/chromedriver"  
     SELENIUM_HUB_URL = 'http://localhost:4444/wd/hub'
     IMPLICIT_WAIT = 10
